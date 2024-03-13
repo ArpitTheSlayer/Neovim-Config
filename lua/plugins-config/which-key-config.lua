93d6b6dcc39b258ffea4b63a41e772057c7a4625
Arpit Pandey <arpitpandey.2001@gmail.com>
2024-03-13 14:01:06 +0530

    Keymaps added in which key plugin

--- lua/plugins-config/which-key-config.lua
+++ lua/plugins-config/which-key-config.lua
@@ -1,4 +1,29 @@
 local wk = require("which-key")
 wk.register({
-	e = "Toggle Explorer"
+	w = "Write file",
+	q = "Quit file",
+	d = "Discard changes",
+	e = "Toggle explorer",
+	r = "Refresh lua files",
+	l = "Open lazy",
+	m = "Open mason",
+	t = "Open treesitter",
+	v = "Vertical split",
+	h = "Horizontal split",
+	f = {
+		name = "Find",
+		f = "Files",
+		g = "Live grep",
+		b = "Buffers",
+		h = "Help tags",
+	},
 }, { prefix = "<leader>" })
+
+wk.register({
+	["<C-h>"] = "Go to left window",
+	["<C-j>"] = "Go to down window",
+	["<C-k>"] = "Go to up window",
+	["<C-l>"] = "Go to right window",
+	["<Tab>"] = "Go to next buffer",
+	["<S-Tab>"] = "Go to previous buffer",
+})
